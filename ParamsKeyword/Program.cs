394e72b4ba791e8064806e3f8c2ced909a26cc1c
Ethan <78313955+Ethan-Myles@users.noreply.github.com>
2024-12-29 11:20:50 +0000

    Add project files.

---
+++ ParamsKeyword/Program.cs
@@ -0,0 +1,29 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        HashSet<int> numbers = new HashSet<int> {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        double average = CalculateAverage(numbers);
+
+        Console.WriteLine(average);
+
+        /*Before C# 13
+        double CalculateAverage(params int[] numbers)
+        {
+            return numbers.Average();
+        }
+
+        New to C# 13
+        double CalculateAverage(params List<int> numbers)
+        {
+            return numbers.Average();
+        }*/
+
+        //Flexibiity
+        double CalculateAverage(params HashSet<int> numbers)
+        {
+            return numbers.Average();
+        }
+    }
+}
